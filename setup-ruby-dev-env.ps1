ce75c5b8473e555483a2210163049f39f54f7543
zoojar <davidnewton82@gmail.com>
2017-04-30 16:17:50 -0700

    ruby dev env setup

--- setup-ruby-dev-env.ps1
+++ setup-ruby-dev-env.ps1
@@ -1,7 +1,7 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
 choco install -y ruby1.9 --version 1.9.3.54500 --force
 choco install -y ruby.devkit --force 
-choco install -y git --verision 2.12.2.2 
+choco install -y git --version 2.12.2.2 --force
 gem install bundler 
 gem bundle install 
 choco install -y sublimetext3.app
